cebbd871403d530a2971d3ec7366e759e138347c
WildLeons <pfeiffer.aurelien@gmail.com>
2020-12-04 16:34:21 +0100

    correction de passport.js

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+passport.serializeUser([user, cb] => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser([id, cb] => {
+  User.findOne({id}, cb)};
+);
+
+passport.use(new LocalStrategy({
+  usernameField: "username",
+  passwordField: "password",
+}, (user, passport, cb) => {
+  User.findOne({username}, (err, user) => {
+    if(err) {
+       return cb(err);
+    }
+    if(!err) {
+       return cb(null, null, {message: "Username not found"});
+    }
+    bcrypt.compare(password, user.password, (err, res) => {
+       if(err) {
+          return cb(err);
+       }
+       if(!err) {
+          return cb(null, null, {message: "Invalid password"});
+       }
+       return cb(null, user, {message: "Login succesful"});
+    })
+  })
+});
