424c3b8c86779ab099f468bfba380d1b77f42457
WildLeons <pfeiffer.aurelien@gmail.com>
2020-12-18 14:19:11 +0100

    Ajout correction Utilisation de BDD

--- config/datastores.js
+++ config/datastores.js
@@ -48,10 +48,10 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
-
-  },
+     adapter: 'sails-mysql',
+     //url: 'mysql://user:password@host:port/database'
+     url: process.end.MYSQL_DATABASE_URL,
+   },
 
 
 };
